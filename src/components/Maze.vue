8ea730d1672170596d778e78a00604ea047788ae
Roland Doda <rolanddoda2014@gmail.com>
2020-10-16 17:23:53 +0200

    Create computed property for the closestPath and useful method utilities

--- src/components/Maze.vue
+++ src/components/Maze.vue
@@ -9,13 +9,15 @@
         class="cell"
         v-for="(cell, index) of data.data"
         :class="[
-          cell.join(' '),
           { pony: index === data.pony[0] },
           { domokun: index === data.domokun[0] },
           { finish: index === data['end-point'][0] },
           { 'border-right': index !== 0 && (index + 1) % cols === 0 },
-          { 'border-bottom': index > cols * rows - cols - 1 }
+          { 'border-bottom': index > cols * rows - cols - 1 },
+          { 'border-top': cell.includes('north') },
+          { 'border-left': cell.includes('west') }
         ]"
+        :data-index="index"
         :key="index"
       >
         <span v-if="index === data.pony[0]">🐎</span>
@@ -49,10 +51,61 @@ export default {
     data: null
   }),
 
+  computed: {
+    closestPathToFinish() {
+      if (!this.data) return [];
+      const ponyPos = this.data.pony[0];
+      const finishPos = this.data["end-point"][0];
+      const cells = this.data.data;
+      let pathFound = [];
+
+      if (this.canGoBottom(ponyPos)) {
+        pathFound = this.tryPath(ponyPos + this.cols, finishPos, cells, []);
+      } else if (this.canGoRight(ponyPos)) {
+        pathFound = this.tryPath(ponyPos + 1, finishPos, cells, []);
+      } else if (this.canGoLeft(ponyPos)) {
+        pathFound = this.tryPath(ponyPos - 1, finishPos, cells, []);
+      } else if (this.canGoTop(ponyPos)) {
+        pathFound = this.tryPath(ponyPos - this.cols, finishPos, cells, []);
+      }
+
+      return pathFound;
+    }
+  },
+
   created() {
     this.$axios.get(`/maze/${this.id}`).then(({ data }) => {
       this.data = data;
     });
+  },
+
+  methods: {
+    canGoTop(pos) {
+      const maze = this.data.data;
+      return !maze[pos].includes("north");
+    },
+
+    canGoRight(pos) {
+      const maze = this.data.data;
+      return (pos + 1) % this.cols !== 0 && !maze[pos + 1].includes("west");
+    },
+
+    canGoBottom(pos) {
+      const maze = this.data.data;
+      return (
+        pos + this.cols < maze.length &&
+        !maze[pos + this.cols].includes("north")
+      );
+    },
+
+    canGoLeft(pos) {
+      const maze = this.data.data;
+      return !maze[pos].includes("west");
+    },
+
+    tryPath(pos, finishPos, path) {
+      // TODO implement algorithm
+    }
   }
 };
 </script>
@@ -85,11 +138,11 @@ export default {
         border-bottom: 1px solid;
       }
 
-      &.north {
+      &.border-top {
         border-top: 1px solid;
       }
 
-      &.west {
+      &.border-left {
         border-left: 1px solid;
       }
 
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    clientLogLevel: "warn",
+    overlay: false
+  }
+};
