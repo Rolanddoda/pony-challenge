32abc7adad834d1d87f241ea86902b2c49e74b68
Roland Doda <rolanddoda2014@gmail.com>
2020-12-05 19:52:23 +0100

    Fix issue with setting the amount in local storage for the first time

--- src/functionalities/bet/bet.js
+++ src/functionalities/bet/bet.js
@@ -1,6 +1,6 @@
 import { amount, changeAmount } from '@/functionalities/bet/betState'
 
 export function runBetOnInitialRender() {
-  if (!localStorage.getItem('amount')) localStorage.setItem('amount', `${amount}`)
+  if (!localStorage.getItem('amount')) localStorage.setItem('amount', `${amount()}`)
   else changeAmount(Number(localStorage.getItem('amount')))
 }
